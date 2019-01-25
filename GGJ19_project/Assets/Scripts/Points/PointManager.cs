da7d50ff048093d3e6e72d9c89f81f71d5e064de
idaniel95 <idaniel95@me.com>
2019-01-26 00:17:17 +0100

    addes small point system, started folder structure.

---
+++ GGJ19_project/Assets/Scripts/Points/PointManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PointManager : MonoBehaviour
+{
+    public static PointManager Instance { get { return GetInstance(); } }
+
+    private static PointManager instance;
+
+    private static PointManager GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = FindObjectOfType<PointManager>();
+        }
+        return instance;
+    }
+
+    private List<Point> points;
+
+    private void FindPoints()
+    {
+        points = FindObjectsOfType<Point>().ToList();
+    }
+ 
+    public List<Point> GetPoints(PointType pointType)
+    {
+        return points.FindAll(x => x.pointType == pointType);
+    }
+
+}
