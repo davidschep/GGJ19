083652d0d0036224c346a8f6781c3acecd94f62a
idaniel95 <idaniel95@me.com>
2019-01-26 01:14:27 +0100

    Added enemy patrolling state.

--- GGJ19_project/Assets/Scripts/Enemies/EnemyPatrollingState.cs
+++ GGJ19_project/Assets/Scripts/Enemies/EnemyPatrollingState.cs
@@ -1,13 +1,22 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class EnemyPatrollingState : MonoBehaviour, IState
 {
     public StateType StateType { get { return StateType.EnemyPatrollingState; } }
 
     private List<Point> wayPoints;
+    private NavMeshAgent navMeshAgent;
 
-    public virtual void Act() { }
+    public virtual void Act()
+    {
+        if (!navMeshAgent.pathPending && navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance && !navMeshAgent.hasPath || navMeshAgent.velocity.sqrMagnitude == 0f)
+        {
+            SetRandomWayPointDestination();
+        }
+    }
 
     public virtual void Enter() { }
 
@@ -16,5 +25,12 @@ public class EnemyPatrollingState : MonoBehaviour, IState
     private void Awake()
     {
         wayPoints = PointManager.Instance.GetPoints(PointType.EnemyWayPoint);
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void SetRandomWayPointDestination()
+    {
+        print("set new waypoint");
+        navMeshAgent.SetDestination(wayPoints.GetRandom().transform.position);
     }
 }
--- GGJ19_project/Assets/Scripts/Enemies/EnemyStateManager.cs
+++ GGJ19_project/Assets/Scripts/Enemies/EnemyStateManager.cs
@@ -2,4 +2,12 @@
 
 [RequireComponent(typeof(StateMachine))]
 public class EnemyStateManager : MonoBehaviour
+{
+    private StateMachine stateMachine;
+
+    private void Start()
+    {
+        stateMachine = GetComponent<StateMachine>();
+        stateMachine.SwitchState(StateType.EnemyPatrollingState);
+    }
 }
