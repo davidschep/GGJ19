cc8017bce723f476af72ce637d01b952093f477e
Max <max.mxmail@gmail.com>
2019-01-26 14:05:11 +0100

    NavMesh setup, cat/mouse agents, Movement

--- GGJ19_project/Assets/Scripts/Player/PlayerController.cs
+++ GGJ19_project/Assets/Scripts/Player/PlayerController.cs
@@ -20,8 +20,6 @@ public class PlayerController : MonoBehaviour
     }
 
     private NavMeshAgent agent;
-    private bool isOffLinking = false;
-
     private void Awake() 
     {
         agent = this.GetComponent<NavMeshAgent>();    
@@ -31,12 +29,6 @@ public class PlayerController : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-
-        // if(agent.isOnOffMeshLink && !isOffLinking)
-        // {
-        //     StartCoroutine(MoveAcrossNavMeshLink());
-        //     isOffLinking = true;
-        // }
 
         Rotate(horizontal);
         Move(horizontal, vertical);
@@ -51,18 +43,12 @@ public class PlayerController : MonoBehaviour
 
     void Move(float horizontal, float vertical)
     {
-        // Vector3 input = new Vector3(horizontal * rotationSpeed, 0, vertical);
+        Vector3 input = new Vector3(horizontal, 0, vertical);
 
-        // if (input.magnitude > 1)
-        // {
-        //     input = input.normalized;  
-        // } 
-
-        // Quaternion rotation = Quaternion.Euler(this.transform.eulerAngles);
-        // Vector3 direction = rotation * input;
-
-        //LookAtY(transform.position + direction);
-        //agent.velocity = direction * agent.speed;
+        if (input.magnitude > 1)
+        {
+            input = input.normalized;  
+        }
 
         Vector3 dir = this.transform.forward * vertical;
         agent.velocity = dir * agent.speed;
@@ -73,32 +59,4 @@ public class PlayerController : MonoBehaviour
     {
         transform.Rotate(Vector3.up * (Input.GetAxis("Horizontal") * agent.angularSpeed) * Time.deltaTime);
     }
-
-    // void LookAtY(Vector3 position)
-    // {
-    //     transform.LookAt(new Vector3(position.x, this.transform.position.y, position.z));
-    // }
- 
-    // IEnumerator MoveAcrossNavMeshLink()
-    // {
-    //     OffMeshLinkData data = agent.currentOffMeshLinkData;
-    //     agent.updateRotation = false;
-
-    //     Vector3 startPos = agent.transform.position;
-    //     Vector3 endPos = data.endPos + Vector3.up * agent.baseOffset;
-    //     float duration = (endPos-startPos).magnitude / agent.velocity.magnitude;
-    //     float t = 0.0f;
-    //     float tStep = 1.0f/duration;
-    //     while(t<1.0f)
-    //     {
-    //         transform.position = Vector3.Lerp(startPos,endPos,t);
-    //         agent.destination = transform.position;
-    //         t+=tStep*Time.deltaTime;
-    //         yield return null;
-    //     }
-    //     transform.position = endPos;
-    //     agent.updateRotation = true;
-    //     agent.CompleteOffMeshLink();
-    //     isOffLinking= false; 
-    // }
 }
